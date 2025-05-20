c9513aa2d3d17744d8598b9485956aa7d1d09abd
YooJaeHyeok <yoojh5685@gmail.com>
2025-05-20 22:14:22 +0900

    A 수정

--- ViewController.swift
+++ ViewController.swift
@@ -2,10 +2,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("안녕하세요")
-            .font(.system(size: 24))
+        Text("A의 라벨입니다") // 이 부분을 수정
+	            .font(.system(size: 30)) // 이 부분을 수정
             .multilineTextAlignment(.center)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.white)
+            .background(Color.red) // 이 부분을 수정
     }
 }
